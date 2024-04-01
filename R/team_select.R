b1106e7b04c0c48525841adfeabb64311f51a568
djperrenoud <a02329412@usu.edu>
2024-04-01 16:08:05 -0600

    data-raw folder

--- R/team_select.R
+++ R/team_select.R
@@ -15,12 +15,15 @@
 #' @export
 team_select <- function(team, year) {
   # Load the dataset
-  dataset <- read.csv("dataset.csv")
+  dataset <- read.csv("data-raw/dataset.csv")
 
   # Subset the dataset based on the provided team and year
-  subset_data <- dataset[as.character(dataset$TEAM) == team & dataset$YEAR == year, ]
+  # NOTE: We need to flag them somehow if they type the team name wrong
+  subset_data <- dataset[as.character(dataset$TEAM) ==
+    team & dataset$YEAR == year, ]
 
   # Return the subsetted data
+  # Also this won't be as messy bc we clean our data to only fit what we need
   return(new_teamdata(subset_data))
 }
 
@@ -30,5 +33,3 @@ team_select <- function(team, year) {
 #' Note we could also create a team_matchup function that could be used
 #' to compare two teams in whatever metric we want. This could be another
 #' custom S3 object we do OR we could just throw two of them into each function.
-
-
