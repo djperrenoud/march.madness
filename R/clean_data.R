2b5b7525bb23ad62e57bbc569975245ea5d8abb9
JonahFeigleson <jonahfeigleson13@gmail.com>
2024-04-07 18:21:20 -0600

    Added data cleaning script

---
+++ R/clean_data.R
@@ -0,0 +1,8 @@
+#Import csv as dataframe.
+raw_data <- read.csv("data-raw/dataset.csv")
+
+#Subset data to only include columns holding stats of interest to our analysis.
+clean_data <- raw_data[, c(1,2,8,9,10,13,17,21,25,26,27,29:64)]
+
+#Export the dataframe as a new csv file.
+write.csv(clean_data, file = "initial_clean", row.names = FALSE)
